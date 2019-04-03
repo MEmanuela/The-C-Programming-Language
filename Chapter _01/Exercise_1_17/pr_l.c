5aef0c8280dc7bdfcd4c38ac569c80e22e4fe29f
Ema <mariaemanuelatriboi@gmail.com>
2019-04-03 09:55:15 +0300

    ex 17 -- done

---
+++ Chapter _01/Exercise_1_17/pr_l.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define MAXLINE 1000    /* maximum input line size */
+
+int getln(char line[], int maxline);
+
+/* print longest input line */
+
+int main()
+{
+    int len;                /* current line length */
+    char line[MAXLINE];     /* current input line */
+
+    while((len = getln(line,MAXLINE)) > 0)
+    {
+        if (len > 80)
+
+            printf("%s", line);
+    }
+    return 0;
+}
+
+int getln(char line[], int maxline)
+{
+    int c, i;
+    for(i = 0; i < maxline - 1 && (c = getchar()) != EOF && c !='\n'; i++)
+    {
+        line[i] = c;
+    }
+    if (c == '\n')
+    {
+        line[i] = c;
+        ++i;
+    }
+    line[i] = '\0';
+    
+    return i;
+}
